6585f9c36e7eaef20f01d68f8d18bfcd813649eb
Denis V. Dimchev <denis.dimchev@gmail.com>
2022-05-18 10:55:22 +0200

    Update MacD.py

--- MacD.py
+++ MacD.py
@@ -7,14 +7,12 @@ import freqtrade.vendor.qtpylib.indicators as qtpylib
 
 class MacD(IStrategy):
     # This strategy does not use crossovers but just enters/exits trades
-    # when 'is above' / 'is under' conditions are met.
     timeframe = "1m"  #1m, 5m, 1h, 1d
     stoploss = -1
     minimal_roi = {"0": 100.0}
 
  # --- Plotting ---
 
-    # Use this section if you want to plot the indicators on a chart after backtesting
     plot_config = {
         'main_plot': {
             'signal': {'color': 'blue'},
@@ -39,7 +37,6 @@ class MacD(IStrategy):
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                # Buy when close price > sma and rsi > rsi_bline (no crossover signal, just above...)
                 (dataframe['close'] == dataframe['signal'])
             ), 'buy'] = 1
 
@@ -48,7 +45,6 @@ class MacD(IStrategy):
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                # Sell when close price < sma and rsi < rsi_sline (no crossover signal, just below...)
                 (dataframe['close'] <= dataframe['signal'])
             ), 'sell'] = 1
 
